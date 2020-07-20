4b1546053cbf01299524a751acd6c60ce46d200d
amandafdal <amandafdal@gmail.com>
2020-07-20 20:40:25 -0300

    :art:recac consts and add firebase.firestore, Co-authored-by: coder.anac@gmail.com

--- src/views/Register/Register.jsx
+++ src/views/Register/Register.jsx
@@ -1,28 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import firebase from '../../firebase';
 import { Link } from "react-router-dom";
 import { withRouter } from 'react-router-dom';
 
 function Register(props) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   function signUp(e) {
     e.preventDefault();
-
-    const name = e.currentTarget.name.value
-    const email = e.currentTarget.email.value
-    const password = e.currentTarget.password.value
-
+    const service =  e.currentTarget.service.value;
     firebase.auth()
       .createUserWithEmailAndPassword(email, password)
       .then(
-        res => {
-          const user = firebase.auth().currentUser;
-          user.updateProfile({
-            displayName: name,
-          })
+        () => {
+          const user = firebase.auth().currentUser.uid;
+          firebase.firestore().collection('employee').doc(user).set({
+            name: name,
+            service: service,
+          }) 
         }
       )
       .then(
-        res => {
+          () => {
           props.history.push('./home');
         }
       )
@@ -32,18 +33,18 @@ function Register(props) {
   return (
     <>
       <form onSubmit={signUp}>
-        <input type="text" name="name" autoComplete="nome" placeholder="Nome" />
-        <input type="email" name="email" autoComplete="email" placeholder="E-mail" />
-        <input type="password" name="password" autoComplete="password" placeholder="Password" />
-        <button type="submit">Entrar</button>
+        <input type="text" name="name" autoComplete="nome" placeholder="Nome" onChange={(e) => setName(e.target.value)} />
+        <input type="email" name="email" autoComplete="email" placeholder="E-mail" onChange={(e) => setEmail(e.target.value)} />
+        <input type="password" name="password" autoComplete="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
         <label>
-          <input type="radio" name="service" value="attendence" /> 
+          <input type="radio" name="service" value="Atendimento" /> 
           Atendimento
         </label>
         <label>
-          <input type="radio" name="service" value="kitchen" /> 
+          <input type="radio" name="service" value="Cozinha" /> 
           Cozinha
         </label>
+        <button type="submit">Entrar</button>
       </form>
       <p>Já tem uma conta? <Link to="/login">Faça login</Link></p>
     </>
