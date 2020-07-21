99d90ecf4d6110d4fc93a3063b1e465919c2ee8c
coderanac <coder.anac@gmail.com>
2020-07-21 18:29:30 -0300

    :lipstick: add Login Co-authored-by: amandafdal@gmail.com

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -3,6 +3,8 @@ import firebase from '../../firebase';
 import { withRouter } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import serviceRedirect from '../../serviceRedirect';
+import Input from '../../components/Input/Input';
+import Button from '../../components/Buttons/SignButton';
 
 function Login(props) {
   function signIn(e) {
@@ -21,9 +23,9 @@ function Login(props) {
   return (
     <>
       <form onSubmit={signIn}>
-        <input type="email" name="email" autoComplete="email" placeholder="E-mail" />
-        <input type="password" name="password" autoComplete="password" placeholder="Password" />
-        <button type="submit">Entrar</button>
+        <Input label = "E-mail"type="email" name="email" autoComplete="email" />
+        <Input label = "Senha"type="password" name="password" autoComplete="password" />
+        <Button type="submit" text="Entrar" /> 
       </form>
       <p> Não tem uma conta? <Link to="/register">Cadastre-se</Link></p>
     </>
