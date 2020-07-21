ae335d36e413d450305f29b34b516df495fae916
coderanac <coder.anac@gmail.com>
2020-07-21 16:45:31 -0300

    :recycle: refactor service routes

--- src/App.js
+++ src/App.js
@@ -4,51 +4,19 @@ import Register from './views/Register/Register';
 import Login from './views/Login/Login';
 import HomeKitchen from './views/HomeKitchen/HomeKitchen';
 import HomeAttendence from './views/HomeAttendence/HomeAttendence';
-import firebase from './firebase';
 
 function App() {
-  /* let serviceRoute;
-  firebase.auth().onAuthStateChanged(user => {
-    if(user){
-      const uid = firebase.auth().currentUser.uid;
-      const docService = firebase.firestore().collection('employee').doc(uid).get().then(doc => doc.data().service);
-      docService === 'Cozinha' ? serviceRoute = HomeKitchen : serviceRoute = HomeAttendence;
-    }
-    serviceRoute = '/';
-  }) */
-
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" render={Login} />
         <Route path="/register" render={Register} />
         <Route path="/login" render={Login} />
-        <Route path="/HomeKitchen" render={HomeKitchen} />
+        <Route path="/kitchen" render={HomeKitchen} />
+        <Route path="/attendence" render={HomeAttendence} />
       </Switch>
     </BrowserRouter>
   );
 }
 
 export default App;
-
-/* 
-import React from 'react';
-import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
-import Register from './views/Register/Register';
-import Login from './views/Login/Login';
-import Home from './views/Home/Home';
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/register" render={Register} />
-        <Route path="/login" render={Login} />
-        <Route path="/home" render={Home} />
-      </Switch>
-    </BrowserRouter>
-  );
-}
-
-export default App;
- */
---
+++ src/serviceRedirect.js
@@ -0,0 +1,17 @@
+import firebase from './firebase';
+
+const serviceRedirect = (props) => {
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      const uid = firebase.auth().currentUser.uid;
+      firebase.firestore().collection('employee').doc(uid).get()
+        .then((doc) => doc.data().service)
+        .then((service) => service === 'chef'
+          ? props.history.push('./kitchen')
+          : props.history.push('./attendence')
+        );
+    }
+  });
+};
+
+export default serviceRedirect;
