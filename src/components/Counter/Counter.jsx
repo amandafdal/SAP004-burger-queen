64e8a46012ef0302efc4e00e5896d6e249914824
amandafdal <amandafdal@gmail.com>
2020-08-01 19:26:55 -0300

    correcting cart state

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import CounterButton from './CounterButton';
+import React from 'react';
 import styled from 'styled-components';
 
 const CounterInput = styled.input`
@@ -22,70 +21,12 @@ const CounterInput = styled.input`
 `;
 
 const Counter = (props) => {
-  const [cart, setCart] = useState({});
-
-  const addToCart = (item) => {
-    let product;
-    if (item.id in cart) {
-      const itemCart = cart[item.id];
-      product = {
-        ...cart,
-        [item.id]: {
-          ...itemCart,
-          quantidade: parseInt(itemCart.quantidade) + 1
-        }
-      }
-    } else {
-      product = {
-        ...cart,
-        [item.id]: {
-          nome: item.data().nome,
-          valor: item.data().valor,
-          quantidade: 1,
-        }
-      }
-    }
-    setCart(product);
-  };
-
-  const delFromCart = (item) => {
-    let product;
-    if (item.id in cart && cart[item.id].quantidade > 0) {
-      const itemCart = cart[item.id];
-      product = {
-        ...cart,
-        [item.id]: {
-          ...itemCart,
-          quantidade: itemCart.quantidade - 1
-        }
-      }
-    } else {
-      product = { ...cart }
-      delete product[item.id]
-    }
-    setCart(product);
-  };
-
-  const setAmount = (item, value) => {
-    setCart({
-      ...cart,
-      [item.id]: {
-        nome: item.data().nome,
-        quantidade: value,
-      }
-    })
-  }
-
   return (
-    <>
-    < CounterButton handleClick={() => addToCart(props.item)} value='+'/>
     <CounterInput
       type="number" min = "0"
-      value={(cart[props.item.id] && cart[props.item.id].quantidade) || 0} 
-      onChange={(e) => setAmount(props.item, e.currentTarget.value)}
+      value={props.value} 
+      onChange={props.handleChange}
     />
-    < CounterButton handleClick={() => delFromCart(props.item)} value='-'/>
-    </>
   )
 }
 
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,8 +3,9 @@ import firebase from '../firebase';
 /* import NavMenu from '../components/NavMenu'; */
 import Counter from './Counter/Counter';
 import styled from 'styled-components';
+import CounterButton from '../components/Counter/CounterButton';
 
-const Section = styled.section `
+const Section = styled.section`
   width: 100%;
   height: fit-content;
   padding-top: 20px;
@@ -13,7 +14,7 @@ const Section = styled.section `
   align-items: center;
 `;
 
-const MenuContainer = styled.section `
+const MenuContainer = styled.section`
   width: 80%;
   height: fit-content;
   display: flex;
@@ -25,7 +26,7 @@ const MenuContainer = styled.section `
   border: 10px solid #F2B90C;
 `;
 
-const MenuTitle = styled.p `
+const MenuTitle = styled.p`
   background-color: #F2B90C;
   width: 100%;
   text-align: center;
@@ -35,12 +36,12 @@ const MenuTitle = styled.p `
   font-weight: 700;
 `;
 
-const MenuCategory = styled.p `
+const MenuCategory = styled.p`
   font-size: 24px;
   font-weight: 700;
 `;
 
-const MenuTable = styled.table `
+const MenuTable = styled.table`
   padding: 10px;
   font-size: 20px;
   font-weight: 600;
@@ -52,10 +53,10 @@ const MenuTable = styled.table `
   }
 `;
 
-
 const Menu = () => {
   /* const [categories, setCategories] = useState([]); */
   const [menu, setMenu] = useState({});
+  const [cart, setCart] = useState({});
 
   const getFoods = () => {
     firebase.firestore().collection('foods').get()
@@ -77,54 +78,123 @@ const Menu = () => {
 
   useEffect(() => getFoods(), [/* setCategories,  */setMenu]);
 
+  const addToCart = (item) => {
+    let product;
+    if (item.id in cart) {
+      const itemCart = cart[item.id];
+      product = {
+        ...cart,
+        [item.id]: {
+          ...itemCart,
+          quantidade: parseInt(itemCart.quantidade) + 1
+        }
+      }
+    } else {
+      product = {
+        ...cart,
+        [item.id]: {
+          nome: item.data().nome,
+          valor: item.data().valor,
+          quantidade: 1,
+        }
+      }
+    }
+    setCart(product);
+  };
+
+  const delFromCart = (item) => {
+    let product;
+    if (item.id in cart && cart[item.id].quantidade > 0) {
+      const itemCart = cart[item.id];
+      product = {
+        ...cart,
+        [item.id]: {
+          ...itemCart,
+          quantidade: itemCart.quantidade - 1
+        }
+      }
+    } else {
+      product = { ...cart }
+      delete product[item.id]
+    }
+    setCart(product);
+  };
+
+  const setAmount = (item, value) => {
+    setCart({
+      ...cart,
+      [item.id]: {
+        nome: item.data().nome,
+        quantidade: value,
+      }
+    })
+  }
+
+  console.log(cart)
+
   return (
     <>
-    {/* <NavMenu cat={categories} /> */}
-    <Section> 
-      <MenuContainer>
-        <MenuTitle>CAFÉ DA MANHÃ</MenuTitle>
-        {Object.keys(menu).map(
-          (key) => (
-            key === 'breakfast' &&
-            <MenuTable id={key}>
-              <tbody>
-              {menu[key].map((item) => (
-                  <tr>
-                    <td>{item.data().nome}</td>
-                    <td>R$ {item.data().valor}</td>
-                    <td>< Counter item={item}/></td>
-                  </tr>
-                ))}
-              </tbody> 
-            </MenuTable>
+      {/* <NavMenu cat={categories} /> */}
+      <Section>
+        <MenuContainer>
+          <MenuTitle>CAFÉ DA MANHÃ</MenuTitle>
+          {Object.keys(menu).map(
+            (key) => (
+              key === 'breakfast' &&
+              <MenuTable id={key}>
+                <tbody>
+                  {menu[key].map((item) => (
+                    <tr>
+                      <td>{item.data().nome}</td>
+                      <td>R$ {item.data().valor}</td>
+                      <td>
+                        < CounterButton handleClick={() => addToCart(item)} value='+' />
+                        <Counter
+                          type="number" min="0"
+                          value={(cart[item.id] && cart[item.id].quantidade) || 0}
+                          handleChange={(e) => setAmount(item, e.currentTarget.value)}
+                        />
+                        < CounterButton handleClick={() => delFromCart(item)} value='-' />
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </MenuTable>
             )
           )
-        }
-      </MenuContainer>
-      <MenuContainer>
-        <MenuTitle>MENU PRINCIPAL</MenuTitle>
-        {Object.keys(menu).map(
-          (key) => (
-            key !== 'breakfast' && key !== 'adicionais' &&
-            <>
-            <MenuCategory>{key}</MenuCategory>
-            <MenuTable id={key}>
-              <tbody>
-              {menu[key].map((item) => (
-                  <tr>
-                    <td>{item.data().nome}</td>
-                    <td>R$ {item.data().valor}</td>
-                    <td>< Counter item={item}/></td>
-                  </tr>
-                ))}
-              </tbody> 
-            </MenuTable>
-            </>
+          }
+        </MenuContainer>
+        <MenuContainer>
+          <MenuTitle>MENU PRINCIPAL</MenuTitle>
+          {Object.keys(menu).map(
+            (key) => (
+              key !== 'breakfast' && key !== 'adicionais' &&
+              <>
+                <MenuCategory>{key}</MenuCategory>
+                <MenuTable id={key}>
+                  <tbody>
+                    {menu[key].map((item) => (
+                      <tr>
+                        <td>{item.data().nome}</td>
+                        <td>R$ {item.data().valor}</td>
+                        <td>
+                        < CounterButton handleClick={() => addToCart(item)} value='+' />
+                        <Counter
+                          value={(cart[item.id] && cart[item.id].quantidade) || 0}
+                          handleChange={(e) => setAmount(item, e.currentTarget.value)}
+                        />
+                        < CounterButton handleClick={() => delFromCart(item)} value='-' />
+                      </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </MenuTable>
+              </>
             )
           )
-        }
-      </MenuContainer>
-    </Section>
+          }
+        </MenuContainer>
+      </Section>
     </>
   )
 }
