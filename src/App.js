830977da3460d396c3050388c2008f0b91d27958
amandafdal <amandafdal@gmail.com>
2020-08-08 20:49:16 -0300

    :lock: add PrivateRoute

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import Register from './views/Register';
 import Login from './views/Login';
 import HomeKitchen from './views/HomeKitchen';
 import HomeAttendence from './views/HomeAttendence';
 
 function App() {
+  const PrivateRoute = ({component: Component, ...rest}) => {
+    return (
+      <Route {...rest} render={props => (
+        localStorage.user ?
+          <Component {...props} />
+        : <Redirect to="/login" />
+      )} />
+    );
+};
+
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" render={Login} />
         <Route path="/register" render={Register} />
         <Route path="/login" render={Login} />
-        <Route path="/kitchen" render={HomeKitchen} />
-        <Route path="/attendence" render={HomeAttendence} />
+        <PrivateRoute path="/kitchen" component={HomeKitchen} />
+        <PrivateRoute path="/attendence" component={HomeAttendence} />
       </Switch>
     </BrowserRouter>
   );
