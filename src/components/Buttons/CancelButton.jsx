455787c0b52efb32e07b8736cad07d9dd33540ec
amandafdal <amandafdal@gmail.com>
2020-08-11 18:32:37 -0300

    add button behavior

--- src/components/Buttons/CancelButton.jsx
+++ src/components/Buttons/CancelButton.jsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 const StyledBtn = styled.button`
   background-color: #F21F0C;
   outline: none;
-  border: none;
+  border: 2px solid #F21F0C;
   border-radius: 10px;
   width: max-content;
   height: 50px;
@@ -14,6 +14,11 @@ const StyledBtn = styled.button`
   font-size: 24px;
   font-weight: 700;
   cursor: pointer;
+  &:active{
+    color: #F21F0C;
+    background-color: #FFF;
+    transition: 0.3s
+  }
 `;
 
 const CancelButton = (props) =>  <StyledBtn type={props.type} onClick={props.handleClick}> {props.value} </StyledBtn>;
